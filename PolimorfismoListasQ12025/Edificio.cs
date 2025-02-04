19d7605868c07a320b0e9ba3cea03bb3e3d1e37a
Gerardo Perez <113952421+g3rar2@users.noreply.github.com>
2025-02-03 19:09:50 -0600

    Edificio Implementation

---
+++ PolimorfismoListasQ12025/Edificio.cs
@@ -0,0 +1,38 @@
+﻿
+
+namespace PolimorfismoListasQ12025
+{
+    public class Edificio
+    {
+
+        public string Tipo { get; set; }
+        public double Precio { get; set; }
+
+        public Edificio() { }
+
+        public Edificio(string Tipo, double Precio)
+        {
+            this.Tipo = Tipo;
+            this.Precio = Precio;
+        }
+
+        public void Vender ()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Edificio de tipo {Tipo}, ha sido vendido por el precio de L.{Precio.ToString("N2")}");
+        }
+
+        public void Vender(string Vendedor)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Edificio de tipo {Tipo}, ha sido vendido por el precio de L.{Precio.ToString("N2")}, por el vendedor {Vendedor}");
+        }
+
+        public void Vender(string Tipo, double Precio)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Edificio de tipo {Tipo}, ha sido vendido por el precio de L.{Precio.ToString("N2")}");
+        }
+
+    }
+}
--- PolimorfismoListasQ12025/Program.cs
+++ PolimorfismoListasQ12025/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using PolimorfismoListasQ12025;
+
+string tipo, opcion;
+double precio;
+List<Edificio> lista=new List<Edificio> ();
+
+
+do
+{
+
+    Console.Write("Ingrese el tipo de edificio ");
+    tipo = Console.ReadLine ();
+
+    Console.Write("Ingrese el valor de edificio ");
+    precio=Convert.ToDouble (Console.ReadLine());
+
+    Console.Write("Desea Continuar (S/N) o (s/n) ");
+    opcion = Console.ReadLine();
+  
+    Edificio edificio = new Edificio (tipo, precio);
+    lista.Add (edificio);
+
+} while (opcion.ToUpper()!="N");
+
+foreach (Edificio edificio in lista)
+{
+    edificio.Vender();
+}
